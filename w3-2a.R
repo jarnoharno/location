fb255f5d86369325d01be76ac80223d809a36bfc
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-03-29 20:45:34 +0200

    week3 2

--- w3-2a.R
+++ w3-2a.R
@@ -1,6 +1,27 @@
 #!/usr/bin/Rscript
 
 # data
-locations = scan('week3table2.csv', what=integer())
+data = scan('week3table2.csv', what=integer())
+ndata = length(data)
 
+# locations
+locs = sort(unique(data))
+nlocs = length(locs)
 
+# transition matrix
+tm = matrix(0, nlocs, nlocs, dimnames=list(locs,locs))
+# this sucks
+for (i in 1:(ndata-1)) tm[as.character(data[i]),as.character(data[i+1])] = 
+  tm[as.character(data[i]),as.character(data[i+1])] + 1
+
+# total transitions from state
+totaltfrom = apply(tm, 1, sum)
+# fix values for which there is no value
+totaltfrom[totaltfrom == 0] = 1
+
+# mle transition probabilities
+tp = tm / matrix(rep(totaltfrom,nlocs),nlocs)
+
+if (!interactive()) {
+  write.table(format(signif(tp,3)), quote=F)
+}
