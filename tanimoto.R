333ed98f4430a42e6b96c7228a00bee2aa7d0036
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-03-29 13:20:05 +0200

    started week3

---
+++ tanimoto.R
@@ -0,0 +1,23 @@
+#!/usr/bin/Rscript
+
+library(abind)
+
+# data
+rss = read.table('week3table1.csv')
+n = dim(rss)[1]
+
+# filter zeros
+rss[rss == 0] = -100
+
+# tanimoto
+tanimoto = function(a, b) (a %*% b) / (a %*% a + b %*% b - a %*% b)
+
+# successive rows
+succ = abind(rss[1:(n-1),], rss[2:n,], along = 3)
+
+# tanimoto between successive rows
+tsucc = apply(succ, 1, function(x) tanimoto(x[,1], x[,2]))
+
+if (!interactive()) {
+  cat(tsucc, '\n')
+}
